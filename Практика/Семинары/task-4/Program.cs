1b34d512a9099b485ed4510ac0cc77413c26ea93
Alexandra <atoxtaeva@gmail.com>
2024-02-27 20:07:50 +0300

    add 2 task4 seminar2

--- Практика/Семинары/task-4/Program.cs
+++ Практика/Семинары/task-4/Program.cs
@@ -2,22 +2,37 @@
 // конца цифру заданного числа или сообщает, что
 // третьей цифры нет.
 // 456 => 6
-// 7812 => 8
+// 7812 => 1
 // 91 => Третьей цифры нет
+
+// System.Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if(number > 99)
+// {
+//     int result = number % 1000 / 100;
+//     System.Console.WriteLine(result);
+
+// }
+// else
+// {
+//     System.Console.WriteLine("В числе отсутствует третья цифра!");
+// }
+
+// Второй вариант задачи, когда выводим третью цифру с начала, а не с конца.
 
 System.Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 if(number > 99)
 {
-    int result = number % 1000 / 100;
-    System.Console.WriteLine(result);
-
+    while(number > 999)
+    {
+        number /= 10;
+    }
+    System.Console.WriteLine(number % 10);
 }
 else
 {
     System.Console.WriteLine("В числе отсутствует третья цифра!");
 }
-
-// Второй вариант задачи, когда выводим третью цифру с начала, а не с конца.
-
--- Практика/Семинары/task-4/obj/Debug/net8.0/task-4.AssemblyInfo.cs
+++ Практика/Семинары/task-4/obj/Debug/net8.0/task-4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task-4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dafe8781695d097af9249d44e4b0924f4a48932")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7aa4410eac8aabc0840b3a2bb63c21fb30ea203")]
 [assembly: System.Reflection.AssemblyProductAttribute("task-4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task-4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
