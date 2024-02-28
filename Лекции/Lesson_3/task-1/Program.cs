3673b15b7becbe93e08e53e96649cb259f04a009
Alexandra <atoxtaeva@gmail.com>
2024-02-29 01:20:48 +0300

    add coment

--- Лекции/Lesson_3/task-1/Program.cs
+++ Лекции/Lesson_3/task-1/Program.cs
@@ -5,6 +5,6 @@
     double result = numenator / denomenator;
     return result;
 }
-
+// Для вывода значения создадим переменную, в которой сохраним результат и выведем значение результата на экран.
 double result = CalculateFormula(1, 2, 3, 4);
 Console.WriteLine(result);
