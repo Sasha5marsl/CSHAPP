96234ba612553415d45dcddf604a36744f79ab4c
Alexandra <atoxtaeva@gmail.com>
2024-03-05 11:11:13 +0300

    add task-3 lesson-3

---
+++ Лекции/Lesson_3/task-3/Program.cs
@@ -0,0 +1,25 @@
+﻿void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        System.Console.Write($"{e} ");
+    }
+}
+
+int[] array = { 22, 24, 30, 40, 50 };
+ZeroEvenElements(array);
+PrintArray(array);
+
+// Мы создали функцию, которая обнулит чётные элементы массива. И затем вывели значения на экран.
+  
